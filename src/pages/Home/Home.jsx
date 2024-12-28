4f3801a4d05641111cc102bc29525ad2b8dee2cf
dylan12manley <dylan.manley@gmail.com>
2024-12-28 15:25:13 -0800

    add router, build home and about pages, create files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './home.css';
+export default function Home() {
+
+    return (
+        <main>
+            <h1>JJSM Co.</h1>
+            <h2>Consulting and Construction by Jaya</h2>
+            <h3>EST 2016 - Portland, Or.</h3>
+        </main>
+    )
+}
