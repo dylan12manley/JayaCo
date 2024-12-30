06beddead2818767d3e68a0c495535fe0e4ea617
dylan12manley <dylan.manley@gmail.com>
2024-12-29 20:27:57 -0800

    remove console logs and comments, add if statment to trigger alert

--- src/pages/Forms/HomeForm.jsx
+++ src/pages/Forms/HomeForm.jsx
@@ -6,25 +6,11 @@ export default function HomeForm() {
   const [siteH2, setSiteH2] = useState('');
   const [siteH3, setSiteH3] = useState('');
 
-  useEffect(() => {
-    console.log('siteName', siteName);
-    console.log('siteH2', siteH2);
-    console.log('siteH3', siteH3);
-  }, [siteName, siteH2, siteH3]);
-
   function handleSubmitHome(e) {
     e.preventDefault();
-    // if (e.target[0]?.value != '') setSiteName(e.target[0].value);
-    // if (e.target[1]?.value != '') setSiteH2(e.target[1]?.value);
-    // if (e.target[2]?.value != '') setSiteH3(e.target[2]?.value);
-
-    // console.log(
-    //   JSON.stringify({
-    //     siteName: e.target[0].value,
-    //     homePageH2: e.target[1]?.value,
-    //     homePageH3: e.target[2]?.value,
-    //   })
-    // );
+    if (e.target[0]?.value != '') setSiteName(e.target[0].value);
+    if (e.target[1]?.value != '') setSiteH2(e.target[1]?.value);
+    if (e.target[2]?.value != '') setSiteH3(e.target[2]?.value);
 
     fetch('http://localhost:4000/homePage', {
       method: 'POST',
@@ -39,14 +25,11 @@ export default function HomeForm() {
     })
       .then((response) => {
         console.log(response);
-      })
-      .then((data) => {
-        // Handle the response data
-        console.log(data);
-        alert('posted to api!');
+        if ((response.stats = 200)) {
+          alert('Data has been successfully added to the database.');
+        }
       })
       .catch((error) => {
-        // Handle any errors
         console.error(error);
       });
   }
@@ -80,6 +63,11 @@ export default function HomeForm() {
       </div>
       <div className='form-input'>
         <button type='submit'>Send</button>
+      </div>
+      <div className='results-preview'>
+        <span>New Site Name: {siteName}</span>
+        <span>New Home Page H2: {siteH2}</span>
+        <span>New Home Page H3: {siteH3}</span>
       </div>
     </form>
   );
