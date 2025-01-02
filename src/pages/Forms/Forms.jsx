6e969e0aaa63dada0959386a2d888406abc3e598
dylan12manley <dylan.manley@gmail.com>
2025-01-01 19:44:10 -0800

    import sign in + up forms

--- src/pages/Forms/Forms.jsx
+++ src/pages/Forms/Forms.jsx
@@ -1,9 +1,15 @@
 import './forms.css';
 import HomeForm from './HomeForm';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import SignOut from './SignOut';
 
 export default function Forms() {
   return (
     <main id='forms'>
+      <SignUp />
+      <SignIn />
+      <SignOut />
       <HomeForm />
     </main>
   );
