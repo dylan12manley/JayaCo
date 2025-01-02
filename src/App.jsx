e523ad47ed77ae3608091e38404374180d1c64ac
dylan12manley <dylan.manley@gmail.com>
2025-01-01 18:56:16 -0800

    add firebase and dotenv to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
