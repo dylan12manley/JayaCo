a8b9f669d913f8e3aa87e01b497624ec37caca73
dylan12manley <dylan.manley@gmail.com>
2025-01-11 19:13:45 -0800

    create re-usable useFetch function

---
+++ src/functions/useFetch.js
@@ -0,0 +1,38 @@
+// table: string
+// method: string
+// bodyObj: obj
+
+/**
+ * Posts data to the database.
+ *
+ * @param {string} table The table of the database.
+ * @param {string} method The method of API call.
+ * @param {obj} bodyObj The key/value pairs for the data to be posted. Optional
+ * @returns {null} null.
+ */
+function useFetch(table = 'generalInfo', method = 'POST', bodyObj = {}) {
+  const postData = async () => {
+    fetch(`http://localhost:4000/${table}`, {
+      method: method,
+      body: JSON.stringify(bodyObj),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        if ((response.stats = 200)) {
+          alert('Data has been successfully added to the database.');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  if (method === 'POST') postData();
+
+  return;
+}
+
+export default useFetch;
