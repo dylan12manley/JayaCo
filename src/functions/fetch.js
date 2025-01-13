f939392b321cf372e77e753bece5707e711cb29e
dylan12manley <dylan.manley@gmail.com>
2025-01-12 19:01:03 -0800

    add fields for data needed on mainData.js, add headers to fetch.js

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -5,14 +5,14 @@ export const GET = (table) => {
   return axios.get(`${apiBaseURL}/${table}`);
 };
 
-// if need for headers etc.
-
-const headers = 'some headers';
+const headers = {
+  'Content-type': 'application/json; charset=UTF-8',
+};
 
 export const POST = (table, bodyObj) => {
   return axios(`${apiBaseURL}/${table}`, {
     method: 'POST',
-    headers,
+    headers: headers,
     bodyObj,
   });
 };
