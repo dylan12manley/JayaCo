d91085d0723c4ad104e139d864c2cd11069ac2b8
dylan12manley <dylan.manley@gmail.com>
2025-01-14 20:36:04 -0800

    remove login stuff from forms page

--- src/pages/Forms/Forms.jsx
+++ src/pages/Forms/Forms.jsx
@@ -1,15 +1,9 @@
 import './forms.css';
 import HomeForm from './HomeForm';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import SignOut from './SignOut';
 
 export default function Forms() {
   return (
     <main id='forms'>
-      <SignUp />
-      <SignIn />
-      <SignOut />
       <HomeForm />
     </main>
   );
