b12397ddcfcc3a0a12bfa214bbe4720ebda34345
dylan12manley <dylan.manley@gmail.com>
2025-01-15 23:22:03 -0800

    render articleForm on forms page

--- src/pages/Forms/Forms.jsx
+++ src/pages/Forms/Forms.jsx
@@ -1,12 +1,13 @@
 import './forms.css';
 import HomeForm from './HomeForm';
 import CategoryForm from './CategoryForm';
-
+import ArticleForm from './ArticleForm';
 export default function Forms() {
   return (
     <main id='forms'>
       <HomeForm />
       <CategoryForm />
+      <ArticleForm />
     </main>
   );
 }
