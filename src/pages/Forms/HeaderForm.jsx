b22da9e2c904406a99d952048d6976aa57966e0f
dylan12manley <dylan.manley@gmail.com>
2025-01-17 15:54:46 -0800

    make headerFooter post to db

--- src/pages/Forms/HeaderForm.jsx
+++ src/pages/Forms/HeaderForm.jsx
@@ -1,31 +1,103 @@
 import './form.css';
-import ColorPicker from '../../components/ColorPicker/ColorPicker';
-import { useSelector, useDispatch } from 'react-redux';
-
-// Background color
-// Text color
-// (maybe not this)Border color + thickness
-// Category page links
-// About us
-// Company logo / homepage link
-// Social media link 1
-// Social media link 2
+import useFetch from '../../functions/useFetch';
 
 export default function HeaderForm() {
-  const headerBG = useSelector((state) => state.headerBG.value);
-  const headerTextColor = useSelector((state) => state.headerTextColor.value);
+  const arrayPlaceholder = ['elm1', 'elm2', 'elm3', 'elm4', 'elm5'];
 
-  const categories = [];
-
+  function handleSubmitFooterHeader(e) {
+    e.preventDefault();
+    const hElmsArr = [];
+    Array.from(document.querySelectorAll('.header-element'))
+      .filter((elm) => elm.checked)
+      .forEach((elm) => hElmsArr.push(elm.value));
+    const headerStyle = Array.from(document.querySelectorAll('select[name="header-style"]')[0]).filter(
+      (elm) => elm.selected === true
+    )[0].value;
+    const fElmsArr = [];
+    Array.from(document.querySelectorAll('.footer-element'))
+      .filter((elm) => elm.checked)
+      .forEach((elm) => fElmsArr.push(elm.value));
+    const footerStyle = Array.from(document.querySelectorAll('select[name="footer-style"]')[0]).filter(
+      (elm) => elm.selected === true
+    )[0].value;
+    const bodyObj = {
+      smallLogoUrl: e.target[0].value,
+      headerElms: hElmsArr.toString(),
+      headerStyle: parseInt(headerStyle),
+      footerElms: fElmsArr.toString(),
+      footerStyle: footerStyle,
+    };
+    useFetch('headerFooter', 'POST', bodyObj);
+  }
   return (
     <main>
-      <h1>Edit the header and footer</h1>
-      <ColorPicker colorFor={'header'} />
-      <div>
-        <span style={{ background: headerBG, color: headerTextColor }}>Example of header text on background</span>
-      </div>
-      <form></form>
-      {/* <form></form> */}
+      <form onSubmit={handleSubmitFooterHeader}>
+        <h2>Edit the header</h2>
+        <div className='form-input'>
+          <label htmlFor='smallLogoUrl'>Small Logo URL:</label>
+          <input
+            type='text'
+            id='smallLogoUrl'
+            name='smallLogoUrl'
+            placeholder='picture-site.com/small-logo.jpg'
+          />
+        </div>
+        <fieldset>
+          <legend>Select elements to be rendered in the header:</legend>
+          {arrayPlaceholder.map((headerElm, i) => {
+            return (
+              <span key={i}>
+                <label htmlFor='headerElm'>{headerElm}</label>
+                <input
+                  type='checkbox'
+                  value={headerElm}
+                  className='header-element'
+                />
+              </span>
+            );
+          })}
+        </fieldset>
+        <div className='form-input'>
+          <label htmlFor='header-style'>Select header style:</label>
+          <select
+            name='header-style'
+            id='header-style'
+          >
+            <option value='1'>style 1</option>
+            <option value='2'>style 2</option>
+            <option value='3'>style 3</option>
+          </select>
+        </div>
+        <fieldset>
+          <legend>Select elements to be rendered in the footer:</legend>
+          {arrayPlaceholder.map((footerElm, i) => {
+            return (
+              <span key={i}>
+                <label htmlFor='footerElm'>{footerElm}</label>
+                <input
+                  type='checkbox'
+                  value={footerElm}
+                  className='footer-element'
+                />
+              </span>
+            );
+          })}
+        </fieldset>
+        <div className='form-input'>
+          <label htmlFor='footer-style'>Select footer style:</label>
+          <select
+            name='footer-style'
+            id='footer-style'
+          >
+            <option value='1'>style 1</option>
+            <option value='2'>style 2</option>
+            <option value='3'>style 3</option>
+          </select>
+        </div>
+        <div className='form-input'>
+          <button type='submit'>Submit Header/Footer Form</button>
+        </div>
+      </form>
     </main>
   );
 }
