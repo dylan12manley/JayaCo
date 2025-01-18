cf676549c417d62ceb8efd889028c1516ea2fcc4
dylan12manley <dylan.manley@gmail.com>
2025-01-17 17:38:24 -0800

    make fonts form store fonts in state with redux

--- src/pages/Forms/FontsForm.jsx
+++ src/pages/Forms/FontsForm.jsx
@@ -1,21 +1,24 @@
 import './form.css';
 import { useState } from 'react';
-import useFetch from '../../functions/useFetch';
+import { useDispatch } from 'react-redux';
+import { selectMainFont } from '../../reducers/mainFontSlice';
+import { selectSecondaryFont } from '../../reducers/secondaryFontSlice';
 
 export default function FontsForm() {
   const [mainFont, setMainFont] = useState('serif');
   const [secondaryFont, setSecondaryFont] = useState('monospace');
+  const dispatch = useDispatch();
 
   function handleSubmitFonts(e) {
     e.preventDefault();
-    if (e.target[0]?.value != '') setMainFont(e.target[0].value);
-    if (e.target[1]?.value != '') setSecondaryFont(e.target[1]?.value);
-
-    const bodyObj = {
-      mainFont: e.target[0].value,
-      secondaryFont: e.target[1]?.value,
-    };
-    // useFetch('general', 'POST', bodyObj);
+    if (e.target[0]?.value != '') {
+      setMainFont(e.target[0].value);
+      dispatch(selectMainFont(e.target[0].value));
+    }
+    if (e.target[1]?.value != '') {
+      setSecondaryFont(e.target[1]?.value);
+      dispatch(selectSecondaryFont(e.target[1].value));
+    }
   }
 
   return (
