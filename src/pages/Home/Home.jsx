1f1cf943883cdbf288a66617a1485646192fab90
dylan12manley <dylan.manley@gmail.com>
2025-01-18 17:10:18 -0800

    adjust how Home uses data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,36 +22,22 @@ export default function Home() {
         setIsLoading(false);
       }
     };
-
     fetchData();
   }, []);
 
-  const bGColor = useSelector((state) => state.bG1.value);
-  const bG2Color = useSelector((state) => state.bG2.value);
-  const bG3Color = useSelector((state) => state.bG3.value);
-  const textColor = useSelector((state) => state.textColor1.value);
+  console.log(data);
 
   return isLoading ? (
     'Loading...'
   ) : (
     <main id='home-page'>
-      <h1>{data[dataLength]?.siteName}</h1>
+      <h1>{data[dataLength]?.homeTitle}</h1>
       <img
-        src={data[dataLength]?.mainImg}
+        src={data[dataLength]?.homeImgUrl}
         alt="Jaya's main photo"
       />
-      <h2>{data[dataLength]?.homePageH2}</h2>
-      <h3>{data[dataLength]?.homePageH3}</h3>
-      <span>Background color is:</span>
-      <span style={{ background: bGColor }}>{bGColor}</span>
-      <br />
-      <span>Background color 2 is:</span>
-      <span style={{ background: bG2Color }}>{bG2Color}</span>
-      <br />
-      <span>Background color 3 is:</span>
-      <span style={{ background: bG3Color }}>{bG3Color}</span>
-      <br />
-      <span style={{ color: textColor, background: bGColor }}>This is the text color: {textColor}</span>
+      <h2>{data[dataLength]?.homeH2}</h2>
+      <h3>{data[dataLength]?.homeH3}</h3>
     </main>
   );
 }
